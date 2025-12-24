e5b4b30de9438ba98d67138a1ac14305122951e3
vijayacityhuntz <vijaya.v@cityhuntz.com>
2025-12-24 17:14:29 +0530

    Payment details and details table details added

---
+++ app/Models/ReservedTableDetail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReservedTableDetail extends Model
+{
+    use HasFactory;
+
+    protected $table = 'reserved_tables_details';
+
+    protected $primaryKey = 'id'; // change if different
+
+    public $timestamps = true; // false if table has no created_at/updated_at
+
+    protected $guarded = [];
+}
